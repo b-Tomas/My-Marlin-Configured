11aed5c0e080b576055fe5e0688b48c2d980d08b
Tomás Badenes <tomasbadenes@gmail.com>
2023-07-25 12:30:28 -0300

    Default extruder PID settings

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -492,9 +492,9 @@
   // If you are using a pre-configured hotend then you can use one of the value sets by uncommenting it
 
   // Ultimaker
-  #define DEFAULT_Kp 22.2
-  #define DEFAULT_Ki 1.08
-  #define DEFAULT_Kd 114
+  // #define DEFAULT_Kp 22.2
+  // #define DEFAULT_Ki 1.08
+  // #define DEFAULT_Kd 114
 
   // MakerGear
   //#define DEFAULT_Kp 7.0
@@ -505,6 +505,11 @@
   //#define DEFAULT_Kp 63.0
   //#define DEFAULT_Ki 2.25
   //#define DEFAULT_Kd 440
+
+  // Autotune results with 100% fan speed at 195°C
+  #define DEFAULT_Kp 19.64
+  #define DEFAULT_Ki 1.05
+  #define DEFAULT_Kd 91.70
 
 #endif // PIDTEMP
 
